9287042584a3d851e3fee55472bffbf7ce131215
Aristeidis Tsachlaris <aristeidis.tsachlaris@fao.org>
2025-06-29 12:50:09 +0300

    Initialize repo

---
+++ src/test/java/gr/uom/employee_pulse_service/EmployeePulseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uom.employee_pulse_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePulseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
