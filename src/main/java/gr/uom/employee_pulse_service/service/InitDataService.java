3ba3394de5fc6f63f6ee4bb29d7dd3bcc6e130a6
Aristeidis Tsachlaris <aristeidis.tsachlaris@fao.org>
2025-06-30 21:26:24 +0300

    Rename service

--- src/main/java/gr/uom/employee_pulse_service/service/InitDataService.java
+++ src/main/java/gr/uom/employee_pulse_service/service/InitDataService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class InitDatService {
+public class InitDataService {
 
     private final OrganizationRepository organizationRepository;
 
