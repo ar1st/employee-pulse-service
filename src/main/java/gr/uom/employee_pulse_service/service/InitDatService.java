7bbe7a1cf1b85027e43f74214ef68e875e1a1c81
Aristeidis Tsachlaris <aristeidis.tsachlaris@fao.org>
2025-06-30 21:24:27 +0300

    Add init data

---
+++ src/main/java/gr/uom/employee_pulse_service/service/InitDatService.java
@@ -0,0 +1,33 @@
+package gr.uom.employee_pulse_service.service;
+
+import gr.uom.employee_pulse_service.model.Organization;
+import gr.uom.employee_pulse_service.repository.OrganizationRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class InitDatService {
+
+    private final OrganizationRepository organizationRepository;
+
+    @PostConstruct
+    public void initData() {
+        Organization organization1 = new Organization();
+        organization1.setName("University of Macedonia");
+        organization1.setLocation("Thessaloniki");
+
+        Organization organization2 = new Organization();
+        organization2.setName("International Hellenic University");
+        organization2.setLocation("Thessaloniki");
+
+        Organization organization3 = new Organization();
+        organization3.setName("BestSecret");
+        organization3.setLocation("Munich");
+        organizationRepository.saveAll(List.of(organization1, organization2, organization3));
+    }
+
+}
